33d5019261bf197ce9d3739a1d01f8e7a88761fa
falandys <mateuszfalandys@gmail.com>
2023-07-08 15:19:42 +0200

    usuwanie fiszek

--- memoryapp/repository.py
+++ memoryapp/repository.py
@@ -11,15 +11,19 @@ cards_list = [
     Card(1, 1, "Drzwi", "Door"),
     Card(2, 1, "Okno", "Window")
 ]
-id_cards=2
+id_cards = 2
+
+
 def get_categories():
     return categories_list
+
 
 def create_category(category_name):
     category = Category(__next_category_id(), category_name)
     categories_list.append(category)
 
     return category
+
 
 def get_category(category_id):
     results = [category for category in categories_list if category.category_id == category_id]
@@ -29,12 +33,14 @@ def get_category(category_id):
     else:
         raise NotFoundException('Category')
 
+
 def delete_category(category_id):
     results = [category for category in categories_list if category.category_id == category_id]
     if results:
         categories_list[:] = [category for category in categories_list if category.category_id != category_id]
     else:
         raise NotFoundException('Category')
+
 
 def get_cards(category_id):
     categories_results = [category for category in categories_list if category.category_id == category_id]
@@ -43,6 +49,7 @@ def get_cards(category_id):
         raise NotFoundException('Category')
     cards_results = [card for card in cards_list if card.category_id == category_id]
     return cards_results
+
 
 def create_card(category_id, word, translation):
     categories_results = [category for category in categories_list if category.category_id == category_id]
@@ -54,10 +61,25 @@ def create_card(category_id, word, translation):
     cards_list.append(card)
     return card
 
+
+def delete_card(category_id, card_id):
+    categories_results = [category for category in categories_list if category.category_id == category_id]
+
+    if not categories_results:
+        raise NotFoundException('Category')
+
+    cards_results = [card for card in cards_list if card.category_id == category_id and card.card_id == card_id]
+    if cards_results:
+        cards_list[:] = [card for card in cards_list if card.category_id != category_id or card.card_id != card_id]
+    else:
+        raise NotFoundException('Card')
+
+
 def __next_category_id():
     global id_categories
-    id_categories +=1
+    id_categories += 1
     return id_categories
+
 
 def __next_card_id():
     global id_cards
--- memoryapp/routes/cards.py
+++ memoryapp/routes/cards.py
@@ -7,6 +7,7 @@ from flask import jsonify, request
 def cards(category_id):
     return jsonify(get_cards(category_id))
 
+
 @app.route('/categories/<int:category_id>/cards', methods=['POST'])
 def add_card(category_id):
     r = request.json
@@ -15,3 +16,10 @@ def add_card(category_id):
     translation = r['translation']
 
     return jsonify(create_card(category_id, word, translation)), 201
+
+
+@app.route('/categories/<int:category_id>/cards/<int:card_id>', methods=['DELETE'])
+def remove_card(category_id, card_id):
+    delete_card(category_id, card_id)
+
+    return '', 204
